e8456a4c69532992e8c481c55548147a980ac128
karthick346t <vigneshkarthick1811@gmail.com>
2025-03-09 23:29:46 +0530

    css14

--- djecommerce/settings/base.py
+++ djecommerce/settings/base.py
@@ -79,7 +79,8 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, "static_in_env")]
 
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static_root')
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media_root')
+MEDIA_ROOT = "/app/media/"
+
 
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
