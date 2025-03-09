e9c50c34e7aef0bdec68e71b6debab658a2f7941
karthick346t <vigneshkarthick1811@gmail.com>
2025-03-09 13:15:46 +0530

    commit

--- djecommerce/settings/base.py
+++ djecommerce/settings/base.py
@@ -36,6 +36,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'allauth.account.middleware.AccountMiddleware',  
 ]
 
 INTERNAL_IPS = ['127.0.0.1']
